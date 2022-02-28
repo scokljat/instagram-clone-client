e33b7322a27fa162f3fb33b46183f65a300a0b14
scokljat <90629541+scokljat@users.noreply.github.com>
2022-03-01 00:28:49 +0100

    Adds sigup

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux";
+import { Flex, Text, Image } from "@chakra-ui/react";
 import SideBar from "./SideBar";
 
 function Profile() {
-  return <SideBar />;
+  const newState = useSelector((state) => state);
+  const user = newState.reducerUsers.users;
+  return (
+    <>
+      <SideBar />
+      <Text>{user.userName}</Text>
+      <Text>{user.firstName}</Text>
+      <Text>{user.lastName}</Text>
+      <Image boxSize="500px" src={user.image} />
+    </>
+  );
 }
 
 export default Profile;
