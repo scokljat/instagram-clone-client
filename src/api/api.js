effedb7a260cf627e8c48af979b781b522b7bd14
scokljat <selma.cokljat@hotmail@domain.com>
2022-05-10 21:07:23 +0200

    Removes getPostsByUserId

--- src/api/api.js
+++ src/api/api.js
@@ -7,5 +7,4 @@ export const login = (user) => axios.post(`${url}/login`, user);
 export const getUser = (id) => axios.get(`${url}/users/${id}`, id);
 
 export const getPosts = () => axios.get(url);
-export const getPostsByUser = (id) => axios.get(`${url}/posted/${id}`, id);
 export const createPost = (newPost) => axios.post(url, newPost);
