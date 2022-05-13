690641cf33b909e53db4adf18e28d40cd1ed81dd
scokljat <selma.cokljat@hotmail.com>
2022-05-13 23:46:01 +0200

    Adds reducerLikes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import { reducerPosts } from "./posts";
 import { authReducer } from "./auth";
 import { reducerUsers } from "./users";
+import { reducerLikes } from "./likes";
 
 const rootReducer = combineReducers({
   reducerPosts: reducerPosts,
   reducerUsers: reducerUsers,
+  reducerLikes: reducerLikes,
   authReducer: authReducer,
 });
 
